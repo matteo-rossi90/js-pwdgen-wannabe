4e695e80b868053115e4cc3284dd26a907c2014c
matteo-rossi90 <matteo.rossi1314@gmail.com>
2024-06-03 15:48:09 +0200

    Correzione codice js

--- js/main.js
+++ js/main.js
@@ -19,9 +19,8 @@ let FavColorUser = prompt('Inserisci qui il tuo colore preferito');
 console.log(FavColorUser);
 
 //restituire il nome della password con nome + cognome + colore + 23
-const NumberID = parseInt(23);
 
-let PwUser = NameUser + SurenameUser + FavColorUser + NumberID;
+let PwUser = NameUser + SurenameUser + FavColorUser + 23;
 
 document.getElementById('PwText').innerHTML = ' La tua password è: ' + PwUser;
 
