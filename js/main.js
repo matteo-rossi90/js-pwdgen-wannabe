0d691b5b761589e934d45297e0c6361940d20a07
matteo-rossi90 <matteo.rossi1314@gmail.com>
2024-06-03 15:13:01 +0200

    Creazione codice js e css

--- js/main.js
+++ js/main.js
@@ -4,9 +4,25 @@ Infine scrivi sulla pagina nomecognomecolorepreferito23
 */
 
 // chiedere all'utente il nome
+let NameUser = prompt('Inserisci qui il tuo nome');
+
+console.log(NameUser);
 
 //chiedere all'utente il cognome
+let SurenameUser = prompt('Inserisci qui il tuo cognome');
+
+console.log(SurenameUser);
 
 //chiedere il colore preferito
+let FavColorUser = prompt('Inserisci qui il tuo colore preferito');
+
+console.log(FavColorUser);
 
 //restituire il nome della password con nome + cognome + colore + 23
+let NumberID = parseInt(23);
+
+let PwUser = NameUser + SurenameUser + FavColorUser + NumberID;
+
+document.getElementById('PwText').innerHTML = ' La tua password è: ' + PwUser;
+
+console.log(PwUser);
