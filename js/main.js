ceb4bdb5be7011e05d0454971bc48d5f4b7c17b2
matteo-rossi90 <matteo.rossi1314@gmail.com>
2024-06-03 13:52:12 +0200

    Impostazione iniziale

---
+++ js/main.js
@@ -0,0 +1,12 @@
+/* Consegna:
+Chiedi all’utente il suo nome, poi chiedi il suo cognome, poi chiedi il suo colore preferito
+Infine scrivi sulla pagina nomecognomecolorepreferito23
+*/
+
+// chiedere all'utente il nome
+
+//chiedere all'utente il cognome
+
+//chiedere il colore preferito
+
+//restituire il nome della password con nome + cognome + colore + 23
